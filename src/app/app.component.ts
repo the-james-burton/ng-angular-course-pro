adcfd715063edb81f927c089a8f2063a6c327227
jimsey <the.james.burton@gmail.com>
2019-05-23 18:31:26 +0100

    ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,17 @@
-import {
-  Component,
-  ViewChild,
-  ViewContainerRef,
-  AfterContentInit,
-  TemplateRef
-} from '@angular/core';
-
-import { AuthFormComponent } from './auth-form/auth-form.component';
-
-import { User } from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
     <div>
-      <div #entry></div>
+      <ng-container
+        [ngTemplateOutlet]="tmpl">
+      </ng-container>
       <!-- different from course prefix with ng -->
-      <ng-template #tmpl let-name let-location="location">
-        {{ name }} : {{ location }}
+      <ng-template #tmpl>
+        Julia : Somewhere, UK
       </ng-template>
     </div>
   `
 })
-export class AppComponent implements AfterContentInit {
-
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-  ngAfterContentInit(): void {
-    this.entry.createEmbeddedView<any>(this.tmpl, {
-      $implicit: 'Julia',
-      location: 'UK, Somewhere'
-    });
-  }
-}
+export class AppComponent {}
