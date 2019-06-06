b0492ea89b16905b211bc0f02f0e98e0e528e78e
jimsey <the.james.burton@gmail.com>
2019-06-06 06:55:43 +0100

    change detection strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.Default,
   template: `
     <div>
-      <example-one></example-one>
-      <example-two></example-two>
-      <example-three></example-three>
+      <button (click)="addProp()">Add property</button>
+      <button (click)="changeUser()">Change user object</button>
+      <button (click)="changeName()">Change name property</button>
+      <div class="users">
+        <example-one [user]="user"></example-one>
+        <example-two [user]="user"></example-two>
+      </div>
     </div>
   `
 })
 export class AppComponent {
+  user: any = {
+    name: 'John Marbles',
+    age: 34,
+    location: 'New York'
+  };
+
+  addProp() {
+    this.user.email = 'someone@somewhere.net';
+  }
+
+  changeName() {
+    this.user.name = 'Jane Wombat';
+  }
+
+  changeUser() {
+    this.user = {
+      name: 'Oscar Penguin',
+      age: 28,
+      location: 'New York '
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
 import { ExampleOneComponent } from './one/one.component';
 import { ExampleTwoComponent } from './two/two.component';
-import { ExampleThreeComponent } from './three/three.component';
-
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ExampleOneComponent,
-    ExampleTwoComponent,
-    ExampleThreeComponent
+    ExampleTwoComponent
   ],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule {}
