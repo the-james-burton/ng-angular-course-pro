47c285723c800efb13d2d7b769b5807a8e1c9f2b
jimsey <the.james.burton@gmail.com>
2019-06-10 06:51:12 +0100

    host binding

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,9 +1,13 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
+
+  @HostBinding('style.border')
+  border: string;
+
   @HostListener('input', ['$event'])
   onkeydown(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
@@ -19,5 +23,10 @@ export class CreditCardDirective {
     }
 
     input.value = numbers.join(' ');
+
+    this.border = '';
+    if (/[^\d]+/.test(trimmed)) {
+      this.border = '1px solid red';
+    }
   }
 }
