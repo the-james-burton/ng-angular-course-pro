5f39be248aa77eafad49b5ac6ae70f20e16d0713
jimsey <the.james.burton@gmail.com>
2019-06-14 06:48:14 +0100

    FormArray add

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { Product } from '../../containers/models/product.interface';
 
@@ -21,7 +21,7 @@ import { Product } from '../../containers/models/product.interface';
           min="10"
           max="1000"
         />
-        <button type="button">
+        <button type="button" (click)="onAdd()">
           Add stock
         </button>
       </div>
@@ -34,4 +34,11 @@ export class StockSelectorComponent {
 
   @Input()
   products: Product[];
+
+  @Output()
+  added = new EventEmitter<any>();
+
+  onAdd() {
+    this.added.emit(this.parent.get('selector').value);
+  }
 }
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -9,7 +9,12 @@ import { Product } from '../models/product.interface';
     <div class="stock-inventory">
       <form [formGroup]="form" (ngSubmit)="onSubmit()">
         <stock-branch [parent]="form"> </stock-branch>
-        <stock-selector [parent]="form" [products]="products"> </stock-selector>
+        <stock-selector
+          [parent]="form"
+          [products]="products"
+          (added)="addStock($event)"
+        >
+        </stock-selector>
         <stock-products [parent]="form"> </stock-products>
 
         <div class="stock-inventory__buttons">
@@ -49,6 +54,11 @@ export class StockInventoryComponent {
       product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
       quantity: new FormControl(stock.quantity || 10)
     });
+  }
+
+  addStock(stock) {
+    const control = this.form.get('stock') as FormArray;
+    control.push(this.createStock(stock));
   }
 
   onSubmit() {
