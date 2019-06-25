6202f7b2c2bf778a1fbfb9e50e6d9fbb53a7bb54
jimsey <the.james.burton@gmail.com>
2019-06-25 07:57:31 +0100

    reset form control

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -40,5 +40,10 @@ export class StockSelectorComponent {
 
   onAdd() {
     this.added.emit(this.parent.get('selector').value);
+    // note difference between reset, patchValue and setValue
+    this.parent.get('selector').reset({
+      product_id: '',
+      quantity: 10
+    });
   }
 }
