92f3671b481eef74d77e957e3fe04d1c5b3ab8dc
jimsey <the.james.burton@gmail.com>
2019-07-05 19:33:03 +0100

    auxiliary navigation api

--- src/app/mail/components/mail-item/mail-item.component.ts
+++ src/app/mail/components/mail-item/mail-item.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input } from '@angular/core';
 
 import { Mail } from '../../models/mail.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mail-item',
   styleUrls: ['mail-item.component.scss'],
   template: `
-    <a
-      class="mail-item"
-      [routerLink]="['', { outlets: { pane: ['message', message.id] } }]"
-      routerLinkActive="active"
-    >
+    <a class="mail-item" (click)="navigateToMessage()">
       <h3>
         {{ message.from }}
         <span>{{ message.timestamp | date: 'shortTime' }}</span>
@@ -20,6 +17,14 @@ import { Mail } from '../../models/mail.interface';
   `
 })
 export class MailItemComponent {
+  constructor(private router: Router) {}
+
   @Input()
   message: Mail;
+
+  navigateToMessage() {
+    // note that doing this means that the 'active' state is not set in the dom
+    // meaning that the style is not applied...
+    this.router.navigate(['', { outlets: { pane: ['message', this.message.id] } }]);
+  }
 }
