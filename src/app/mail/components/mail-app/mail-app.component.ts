87335c137b650722d6754b2a3bd04a9813aa015f
jimsey <the.james.burton@gmail.com>
2019-07-05 19:16:02 +0100

    auxiliary router outlet

--- src/app/mail/components/mail-app/mail-app.component.ts
+++ src/app/mail/components/mail-app/mail-app.component.ts
@@ -10,6 +10,9 @@ import { Component } from '@angular/core';
         (deactivate)="onDeactivate($event)"
       ></router-outlet>
     </div>
+    <div class="mail">
+      <router-outlet name="pane"></router-outlet>
+    </div>
   `
 })
 export class MailAppComponent {
--- src/app/mail/mail.module.ts
+++ src/app/mail/mail.module.ts
@@ -7,18 +7,29 @@ import { MailItemComponent } from './components/mail-item/mail-item.component';
 import { MailAppComponent } from './components/mail-app/mail-app.component';
 import { MailService } from '../mail.service';
 import { MailFolderResolve } from './containers/mail-folder/mail-folder.resolve';
+import { MailViewComponent } from './components/mail-view/mail-view.component';
 
 export const ROUTES: Routes = [
   {
     path: 'folder/:name',
     component: MailFolderComponent,
     resolve: { messages: MailFolderResolve }
+  },
+  {
+    path: 'message/:id',
+    component: MailViewComponent,
+    outlet: 'pane'
   }
 ];
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(ROUTES)],
-  declarations: [MailFolderComponent, MailAppComponent, MailItemComponent],
+  declarations: [
+    MailFolderComponent,
+    MailAppComponent,
+    MailItemComponent,
+    MailViewComponent
+  ],
   providers: [MailService, MailFolderResolve],
   exports: [MailAppComponent]
 })
