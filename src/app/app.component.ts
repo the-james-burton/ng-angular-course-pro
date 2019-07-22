dd7aa57916fc1eb70f48ea42a9b6be400f84c652
jimsey <the.james.burton@gmail.com>
2019-07-22 07:08:55 +0100

    testing attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,25 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div>
+      <label>
+        Credit Card Number
+        <input
+          name="credit-card"
+          type="text"
+          placeholder="Enter your 16-digit card number"
+          credit-card
+        />
+      </label>
+      <label tooltip="3 digits on back of card" #myTooltip="tooltip">
+        Enter your security code
+        <span (mouseover)="myTooltip.show()" (mouseout)="myTooltip.hide()">
+          (?)
+        </span>
+        <input type="text" />
+      </label>
+    </div>
+
+    <div>
       <stock-inventory></stock-inventory>
     </div>
   `
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { StockInventoryModule } from './stock-inventory/stock-inventory.module';
 
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CreditCardDirective, TooltipDirective],
   imports: [BrowserModule, StockInventoryModule],
   bootstrap: [AppComponent]
 })
