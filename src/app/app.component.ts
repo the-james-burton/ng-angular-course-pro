6558e9368f12a885d4f5d89caf0f737e431f4f01
jimsey <the.james.burton@gmail.com>
2019-07-25 06:49:00 +0100

    ngZone runOutsideAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { FoodStoreService } from './food-store/food-store.service';
+import { Component, OnInit, DoCheck, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div>Food Store ({{ (store | async)?.name }})</div>
+    <div>Counter: {{ counter }}</div>
   `
 })
-export class AppComponent {
-  store = this.foodService.getStore();
-  constructor(private foodService: FoodStoreService) {}
+export class AppComponent implements OnInit, DoCheck {
+  counter = 0;
+  constructor(private zone: NgZone) {}
+
+  ngOnInit() {
+    this.zone.runOutsideAngular(() => {
+      for (let i = 0; i < 100; i++) {
+        setTimeout(() => this.counter++);
+      }
+      this.zone.run(() => {
+        setTimeout(() => (this.counter = this.counter), 1000);
+      });
+    });
+  }
+
+  ngDoCheck() {
+    console.log('Change detection has been run!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
-import { FoodStoreModule } from './food-store/food-store.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FoodStoreModule.forRoot({
-      storeId: 10292,
-      storeToken: 'eca938c99a0e9ff91029dc'
-    })
-  ],
+  imports: [BrowserModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
